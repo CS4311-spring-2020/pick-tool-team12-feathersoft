49e0721071da1bbd80ee0b9f67eb044b5aea6a9a
Cris <camolinasim@miners.utep.edu>
2020-01-31 14:01:33 -0700

    looks cuter

--- teamConfiguration.py
+++ teamConfiguration.py
@@ -4,40 +4,51 @@
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
-# WARNING! All changes made in this file will be 
-#
+# WARNING! All changes made in this file will be lost!
+
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-# "research is just a fancy word for looking things up and knowing more about them"
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(413, 600)
+        MainWindow.resize(531, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 10, 501, 71))
+        self.label.setGeometry(QtCore.QRect(100, 10, 321, 71))
         font = QtGui.QFont()
         font.setPointSize(26)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(20, 140, 70, 18))
+        self.checkBox.setGeometry(QtCore.QRect(10, 280, 381, 41))
         self.checkBox.setObjectName("checkBox")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(20, 210, 121, 41))
+        self.label_2.setGeometry(QtCore.QRect(10, 120, 91, 41))
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(20, 290, 281, 41))
+        self.label_3.setGeometry(QtCore.QRect(10, 220, 101, 21))
         self.label_3.setObjectName("label_3")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(60, 420, 271, 81))
+        self.pushButton.setGeometry(QtCore.QRect(130, 350, 261, 81))
         self.pushButton.setObjectName("pushButton")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(130, 120, 261, 51))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setGeometry(QtCore.QRect(130, 200, 261, 51))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 200, 101, 31))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 240, 101, 16))
+        self.label_5.setObjectName("label_5")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 413, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 531, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -53,8 +64,10 @@ class Ui_MainWindow(object):
         self.label.setText(_translate("MainWindow", "Team Configuration"))
         self.checkBox.setText(_translate("MainWindow", "Lead"))
         self.label_2.setText(_translate("MainWindow", "Lead IP address"))
-        self.label_3.setText(_translate("MainWindow", "No. of established connections to the lead\'s IP address"))
+        self.label_3.setText(_translate("MainWindow", "No. of established "))
         self.pushButton.setText(_translate("MainWindow", "Conect"))
+        self.label_4.setText(_translate("MainWindow", "connections to the"))
+        self.label_5.setText(_translate("MainWindow", "Lead\'s IP address"))
 
 
 if __name__ == "__main__":
