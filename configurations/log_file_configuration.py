33003e4f7cec8d0ce7b3f89f9a4a23065aa11876
Jay <jjjames@miners.utep.edu>
2020-02-03 00:32:32 -0700

    Jesus gomez added the graph configuration, tabular configuration, and relationship configuration.

--- configurations/log_file_configuration.py
+++ configurations/log_file_configuration.py
@@ -7,36 +7,36 @@ from PyQt5.QtCore import *
 class LogFileConfigurationWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(50, 50, 800, 600)
+        self.setGeometry(50, 50, 1000, 800)
         self.setWindowTitle("Log File Configuration")
-        #self.setMaximumSize(800,600)
+        self.setMaximumSize(1000,800)
         self.UI()
 
     def UI(self):
 
-        text = QLabel('Log File Configuration', self)
-        text.setFont(QFont('MS Shell Dlg 2', 20))
-        text.move(50,50)
-        vbox = QVBoxLayout()
-        table = QTableWidget(self)
-        table.setColumnCount(5)
-        table.setRowCount(20)
-        table.setHorizontalHeaderItem(0,QTableWidgetItem(QIcon('up-down-arrow.png'), "List Number"))
-        table.setHorizontalHeaderItem(1,QTableWidgetItem(QIcon('up-down-arrow.png'), "Log Entry Timestamp"))
-        table.setHorizontalHeaderItem(2,QTableWidgetItem(QIcon('up-down-arrow.png'), "Log Entry Event"))
-        table.setHorizontalHeaderItem(3,QTableWidgetItem("Vector"))
-        table.setHorizontalHeaderItem(4,QTableWidgetItem(""))
-
-        header = table.horizontalHeader()
+        self.label = QLabel('Log File Configuration', self)
+        self.label.setFont(QFont('MS Shell Dlg 2', 20))
+        self.label.move(50,50)
+        self.vbox = QVBoxLayout()
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(5)
+        self.table.setRowCount(20)
+        self.table.setHorizontalHeaderItem(0,QTableWidgetItem(QIcon('up_arrow.png'), "List Number"))
+        self.table.setHorizontalHeaderItem(1,QTableWidgetItem(QIcon('up_arrow.png'), "Log Entry Timestamp"))
+        self.table.setHorizontalHeaderItem(2,QTableWidgetItem(QIcon('up_arrow.png'), "Log Entry Event"))
+        self.table.setHorizontalHeaderItem(3,QTableWidgetItem("Vector"))
+        self.table.setHorizontalHeaderItem(4,QTableWidgetItem(""))
+        header = self.table.horizontalHeader()
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
 
-        for i in range(table.rowCount()):
-            table.setVerticalHeaderItem(i,QTableWidgetItem(''))
+
+        for i in range(self.table.rowCount()):
+            self.table.setVerticalHeaderItem(i,QTableWidgetItem(''))
         log = [line.split(' ') for line in open('dummy_log.txt').readlines()]
         for i in range(len(log)):
-            table.setItem(i,0,QTableWidgetItem(str(i + 1)))
-            table.setItem(i,1,QTableWidgetItem(str(log[i][0])))
+            self.table.setItem(i,0,QTableWidgetItem(str(i + 1)))
+            self.table.setItem(i,1,QTableWidgetItem(str(log[i][0])))
             checkbox = QTableWidgetItem()
             checkbox.setCheckState(Qt.Unchecked)
             combobox = QComboBox()
@@ -47,14 +47,22 @@ class LogFileConfigurationWindow(QWidget):
             tableitem.setVerticalHeaderItem(1, QTableWidgetItem('Host'))
             tableitem.setVerticalHeaderItem(2, QTableWidgetItem('Source'))
             tableitem.setVerticalHeaderItem(3, QTableWidgetItem('Source Type'))
-            table.setCellWidget(i,2,tableitem)
-            table.setCellWidget(i,3,combobox)
-            table.setItem(i,4,checkbox)
+            self.table.setCellWidget(i,2,tableitem)
+            self.table.setCellWidget(i,3,combobox)
+            self.table.setItem(i,4,checkbox)
 
-        table.setGeometry(50,100,700, 450)
-        vbox.addWidget(table)
+        self.table.clicked.connect(self.header_clicked)
+        self.table.setGeometry(50,100,900, 650)
+        self.vbox.addWidget(self.table)
         #self.setLayout(vbox)
         self.show()
+
+    def header_clicked(self):
+        col = self.table.currentColumn()
+        print(col)
+
+
+
 
 
 if __name__ == '__main__':
