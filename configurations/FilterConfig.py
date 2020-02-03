e539a37d1ce823beafd6149d794c9c7e3e702799
Jay <jjjames@miners.utep.edu>
2020-02-03 00:33:01 -0700

    Jesus gomez added the graph configuration, tabular configuration, and relationship configuration.

---
+++ configurations/FilterConfig.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'FilterConfigjYfehu.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QRect)
+from PySide2.QtGui import (QFont)
+from PySide2.QtWidgets import *
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 20, 171, 31))
+        font = QFont()
+        font.setPointSize(17)
+        font.setBold(True)
+        font.setWeight(75);
+        self.label.setFont(font)
+        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setGeometry(QRect(230, 20, 241, 21))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(30, 60, 58, 16))
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(30, 90, 71, 16))
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(30, 120, 111, 16))
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(30, 150, 111, 16))
+        self.dateTimeEdit = QDateTimeEdit(self.centralwidget)
+        self.dateTimeEdit.setObjectName(u"dateTimeEdit")
+        self.dateTimeEdit.setGeometry(QRect(140, 120, 194, 22))
+        self.dateTimeEdit_2 = QDateTimeEdit(self.centralwidget)
+        self.dateTimeEdit_2.setObjectName(u"dateTimeEdit_2")
+        self.dateTimeEdit_2.setGeometry(QRect(140, 150, 194, 22))
+        self.radioButton = QRadioButton(self.centralwidget)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(110, 90, 100, 20))
+        self.radioButton_2 = QRadioButton(self.centralwidget)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setGeometry(QRect(220, 90, 100, 20))
+        self.radioButton_3 = QRadioButton(self.centralwidget)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.radioButton_3.setGeometry(QRect(340, 90, 100, 20))
+        self.radioButton_4 = QRadioButton(self.centralwidget)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setGeometry(QRect(220, 60, 100, 20))
+        self.radioButton_5 = QRadioButton(self.centralwidget)
+        self.radioButton_5.setObjectName(u"radioButton_5")
+        self.radioButton_5.setGeometry(QRect(110, 60, 100, 20))
+        self.radioButton_6 = QRadioButton(self.centralwidget)
+        self.radioButton_6.setObjectName(u"radioButton_6")
+        self.radioButton_6.setGeometry(QRect(340, 60, 100, 20))
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(30, 200, 112, 32))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Filter Configuration", None))
+        self.plainTextEdit.setPlainText("")
+        self.plainTextEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Keyword Search", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Creator:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Event Type: ", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Start Timestamp:", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"End Timestamp:", None))
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"Red", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"Blue", None))
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"White", None))
+        self.radioButton_4.setText(QCoreApplication.translate("MainWindow", u"Blue", None))
+        self.radioButton_5.setText(QCoreApplication.translate("MainWindow", u"Red", None))
+        self.radioButton_6.setText(QCoreApplication.translate("MainWindow", u"White", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Apply Filter", None))
+    # retranslateUi
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ configurations/VectorConfig.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Vector_ConfiguxAjwP.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.actionEdit_Description = QAction(MainWindow)
+        self.actionEdit_Description.setObjectName(u"actionEdit_Description")
+        self.actionDelete_Vector = QAction(MainWindow)
+        self.actionDelete_Vector.setObjectName(u"actionDelete_Vector")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(60, 20, 231, 31))
+        font = QFont()
+        font.setPointSize(21)
+        font.setBold(True)
+        font.setWeight(75);
+        self.label_3.setFont(font)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(170, 370, 112, 32))
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(290, 370, 112, 32))
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(50, 370, 112, 32))
+        self.tableWidget = QTableWidget(self.centralwidget)
+        if (self.tableWidget.columnCount() < 3):
+            self.tableWidget.setColumnCount(3)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        if (self.tableWidget.rowCount() < 3):
+            self.tableWidget.setRowCount(3)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(1, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(2, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.tableWidget.setItem(0, 0, __qtablewidgetitem6)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.tableWidget.setItem(0, 1, __qtablewidgetitem7)
+        __qtablewidgetitem8 = QTableWidgetItem()
+        self.tableWidget.setItem(1, 0, __qtablewidgetitem8)
+        __qtablewidgetitem9 = QTableWidgetItem()
+        self.tableWidget.setItem(1, 1, __qtablewidgetitem9)
+        __qtablewidgetitem10 = QTableWidgetItem()
+        self.tableWidget.setItem(2, 0, __qtablewidgetitem10)
+        __qtablewidgetitem11 = QTableWidgetItem()
+        self.tableWidget.setItem(2, 1, __qtablewidgetitem11)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(60, 70, 351, 281))
+        self.checkBox = QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(350, 100, 86, 20))
+        self.checkBox_2 = QCheckBox(self.centralwidget)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+        self.checkBox_2.setGeometry(QRect(350, 130, 86, 20))
+        self.checkBox_3 = QCheckBox(self.centralwidget)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+        self.checkBox_3.setGeometry(QRect(350, 160, 81, 20))
+        self.verticalScrollBar = QScrollBar(self.centralwidget)
+        self.verticalScrollBar.setObjectName(u"verticalScrollBar")
+        self.verticalScrollBar.setGeometry(QRect(410, 70, 16, 281))
+        self.verticalScrollBar.setOrientation(Qt.Vertical)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionEdit_Description.setText(QCoreApplication.translate("MainWindow", u"Edit Description", None))
+        self.actionDelete_Vector.setText(QCoreApplication.translate("MainWindow", u"Delete Vector", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Vector Configuration", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Add Vector", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Delete Vector", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Edit Vector", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Vector Name 	\u25bc", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Description 	\u25bc", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Select", None));
+        ___qtablewidgetitem3 = self.tableWidget.verticalHeaderItem(0)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"Vector 1", None));
+        ___qtablewidgetitem4 = self.tableWidget.verticalHeaderItem(1)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"Vector 2", None));
+        ___qtablewidgetitem5 = self.tableWidget.verticalHeaderItem(2)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"Vector 3", None));
+
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        ___qtablewidgetitem6 = self.tableWidget.item(0, 0)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWindow", u"DoS Attack", None));
+        ___qtablewidgetitem7 = self.tableWidget.item(0, 1)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("MainWindow", u"Denial of Service", None));
+        ___qtablewidgetitem8 = self.tableWidget.item(1, 0)
+        ___qtablewidgetitem8.setText(QCoreApplication.translate("MainWindow", u"MitM", None));
+        ___qtablewidgetitem9 = self.tableWidget.item(1, 1)
+        ___qtablewidgetitem9.setText(QCoreApplication.translate("MainWindow", u"Man in the middle", None));
+        ___qtablewidgetitem10 = self.tableWidget.item(2, 0)
+        ___qtablewidgetitem10.setText(QCoreApplication.translate("MainWindow", u"SQL Injection", None));
+        ___qtablewidgetitem11 = self.tableWidget.item(2, 1)
+        ___qtablewidgetitem11.setText(QCoreApplication.translate("MainWindow", u"SQL Injection", None));
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
+
+        self.checkBox.setText("")
+        self.checkBox_2.setText("")
+        self.checkBox_3.setText("")
+    # retranslateUi
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
