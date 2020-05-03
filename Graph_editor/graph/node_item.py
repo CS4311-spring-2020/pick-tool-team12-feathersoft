022a2803242db179add7c894d019e4c0905e2bab
Jay James <38440694+JayJamesJr@users.noreply.github.com>
2020-05-03 13:18:48 -0600

    Add files via upload

--- Graph_editor/graph/node_item.py
+++ Graph_editor/graph/node_item.py
@@ -20,6 +20,7 @@ class NodeItem(QGraphicsItem):
     width: float = 100.0
     height: float = 100.0
     relationships = {}
+    name = None
 
     def __init__(self, pos_x: float, post_y: float, name: str, type: str, parent=None):
         super().__init__(parent)
@@ -28,11 +29,13 @@ class NodeItem(QGraphicsItem):
         self.name = name
         self.type = type
 
+        print(self.pos().x(), self.pos().y(), self.boundingRect().center().x(), self.boundingRect().center().y())
         self.init_ui()
 
     def init_ui(self):
         self.setFlag(QGraphicsItem.ItemIsSelectable)
         self.setFlag(QGraphicsItem.ItemIsMovable)
+        self.setFlag(QGraphicsItem.ItemSendsScenePositionChanges)
         self.setAcceptHoverEvents(True)
 
     '''
@@ -40,7 +43,9 @@ class NodeItem(QGraphicsItem):
     '''
 
     def center_pos(self):
-        return self.boundingRect().center()
+        # grab the bounding rectangle for this class on it's coordinate position
+        center, current = self.boundingRect().center(), self.pos()
+        return QPoint(current.x() + center.x(), current.y() + center.y())
 
     '''
         Add a relationship to the node
@@ -64,11 +69,12 @@ class NodeItem(QGraphicsItem):
         QRectF
             bounding rectangle for this node
     '''
+
     def boundingRect(self) -> QRectF:
         return QRectF(self.pos_x, self.pos_y, self.width, self.height).normalized()
 
     '''
-        This funtion which is usually called by QGraphicsView, paints the contents of an item in local coordinates.
+        This function which is usually called by QGraphicsView, paints the contents of an item in local coordinates.
         
         Parameters
         ----------    
@@ -86,3 +92,12 @@ class NodeItem(QGraphicsItem):
         painter.setPen(QPen(Qt.black))
         painter.setBrush(QBrush(Qt.red))
         painter.drawPath(node_outline.simplified())
+
+    def mouseMoveEvent(self, QGraphicsSceneMouseEvent):
+        super().mouseMoveEvent(QGraphicsSceneMouseEvent)
+        self.boundingRect()
+        self.dragRelationshipItems()
+
+    def dragRelationshipItems(self):
+        for rl in self.relationships.values():
+            rl.dragLine(self.name, self.center_pos())
